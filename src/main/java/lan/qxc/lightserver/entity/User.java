3241c64395fd0515d7358b83949e6cde09f0fca6
qiuxiaochi <1348270542@qq.com>
2020-03-12 23:28:11 +0800

    基本完成用户dao层和service层

--- src/main/java/lan/qxc/lightserver/entity/User.java
+++ src/main/java/lan/qxc/lightserver/entity/User.java
@@ -1,16 +1,34 @@
 package lan.qxc.lightserver.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    private int userid;
+    private Long userid;
+    private String phone;
     private String username;
     private String password;
+    private Byte sex;
+    private String icon;
+
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date birthday;
+
+    private String introduce;
+    private String location;
+    private String hometown;
+    private String job;
+
+    private Byte is_deleted;
+    private Byte is_locked;
 
 }
