77f8b13feb3db111717e1558dda0a282ba5554ae
qiuxiaochi <1348270542@qq.com>
2020-03-27 15:13:55 +0800

    netty  安全认证处理器

---
+++ src/main/java/lan/qxc/lightserver/netty/handler/net_handler/AuthHandler.java
@@ -0,0 +1,32 @@
+package lan.qxc.lightserver.netty.handler.net_handler;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lan.qxc.lightserver.netty.util.SessionUtil;
+
+/*
+    安全认证处理器
+ */
+
+@ChannelHandler.Sharable
+public class AuthHandler extends ChannelInboundHandlerAdapter {
+
+    public static final AuthHandler INSTANCE = new AuthHandler();
+
+    private AuthHandler() {
+
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (!SessionUtil.hasLogin(ctx.channel())) {
+            ctx.channel().close();
+        } else {
+            ctx.pipeline().remove(this);
+            super.channelRead(ctx, msg);
+        }
+    }
+
+}
